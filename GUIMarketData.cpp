1ad77729e51d3aa65ca79ae1a7472eea5c62ba17
cf16 <cf16rocket@gmail.com>
2013-06-18 00:18:29 +0200

    MessageProcessor class has been added. It derives from QThread and will be used as message processor to call PosixClient::processMessages in QThread - this way we should be able to update GUI objects

--- GUIMarketData.cpp
+++ GUIMarketData.cpp
@@ -2,7 +2,7 @@
  * File:   MarketData.cpp
  * Author: Piotr Gregor  postmaster@cf16.eu
  * 
- * Created on May 26, 2013, 7:06 PM
+ * Created on June 16, 2013, 4:06 PM
  */
 #include "MarketData.h"
 #include "GUIMarketData.h"
---
+++ MessageProcessor.h
@@ -0,0 +1,34 @@
+/* 
+ * File:   MessageProcessor.h
+ * Author: Piotr Gregor  postmaster@cf16.eu
+ *
+ * Created on June 17, 2013, 11:57 PM
+ */
+
+#ifndef MESSAGEPROCESSOR_H
+#define	MESSAGEPROCESSOR_H
+
+#include <QtCore/QThread>
+#include <QtCore/QObject>
+#include "PosixClient.h"
+
+class MessageProcessor : public QObject {
+
+    Q_OBJECT
+public:
+    MessageProcessor(boost::shared_ptr<IB::PosixClient> client):client(client){}
+    ~MessageProcessor(){}
+    public Q_SLOTS :
+    void start() {
+        printf("MessageProcessor::processMessages()\n");
+        while (true) {
+            printf("MessageProcessor::processMessages()::while()\n");
+            client->processMessages();
+        }
+    }
+    private:
+        boost::shared_ptr<IB::PosixClient> client;
+};
+
+#endif	/* MESSAGEPROCESSOR_H */
+

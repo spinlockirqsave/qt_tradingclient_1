9f9a4307476d6add3c7447aa287ccc8becbe3daa
cf16 <cf16rocket@gmail.com>
2013-08-03 00:28:39 +0200

    class TradingStrategy has been added. This will be the core of all trading strategies

---
+++ TradingStrategies/TradingStrategy.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   TradingStrategy.cpp
+ * Author: Piotr Gregor  postmaster@cf16.eu
+ * 
+ * Created on August 3, 2013, 12:21 AM
+ */
+
+#include "TradingStrategy.h"
+
+TradingStrategy::TradingStrategy() {
+}
+
+TradingStrategy::TradingStrategy(const TradingStrategy& orig) {
+}
+
+TradingStrategy::~TradingStrategy() {
+}
+
---
+++ TradingStrategies/TradingStrategy.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   TradingStrategy.h
+ * Author: Piotr Gregor  postmaster@cf16.eu
+ *
+ * Created on August 3, 2013, 12:21 AM
+ */
+
+#ifndef TRADINGSTRATEGY_H
+#define	TRADINGSTRATEGY_H
+
+class TradingStrategy {
+public:
+    TradingStrategy();
+    TradingStrategy(const TradingStrategy& orig);
+    virtual ~TradingStrategy();
+private:
+
+};
+
+#endif	/* TRADINGSTRATEGY_H */
+
